060e742209f548a13762e4fd6eefc0e1971a538b
Oz Giat <ozgiat@gmail.com>
2020-11-03 20:22:27 +0200

    critical change

--- hello.py
+++ hello.py
@@ -1,7 +1,2 @@
 print ("Hello  hi  World")
 
-print("another linee#@!&#@!^#@!&")
-print("another linee#@!&#@!^#@!&")
-print("another linee#@!&#@!^#@!&")
-print("another linee#@!&#@!^#@!&")
-print("another linee#@!&#@!^#@!&")
